085a2d17838e859ce02b8a366a6f539804f20628
Anthony Van Cayetano <accayetano4@up.edu.ph>
2022-02-25 19:32:18 +0800

    Basic login, sign up and log out functionalities

--- ProfitMaximizationTool/profitmaximizer/models.py
+++ ProfitMaximizationTool/profitmaximizer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BusinessOwner(User):
+	business_name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.username
--- ProfitMaximizationTool/profitmaximizer/views.py
+++ ProfitMaximizationTool/profitmaximizer/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from profitmaximizer.models import BusinessOwner
 
 
 def index(request):
-	return HttpResponse("Hello test")
+	if request.method == "POST":
+		# sign up
+		if "signup-btn" in request.POST:
+			if request.POST['psw'] == request.POST['confirmpsw']:
+				username = request.POST['username']
+				password = request.POST['psw']
+				business_name = request.POST['businessname']
+
+				business_owner = BusinessOwner.objects.create_user(username=username, email=None, password=password)
+				business_owner.business_name = business_name
+				business_owner.save()
+				login(request, business_owner)
+			else:
+				return render(request, "index.html")
+
+		# sign in
+		if "signin-btn" in request.POST:
+			username = request.POST['username']
+			password = request.POST['psw']
+			business_owner = authenticate(request, username=username, password=password)
+			if business_owner is not None:
+				login(request, business_owner)
+			else:
+				return render(request, "index.html")
+
+		return redirect('/home/')
+
+	return render(request, "index.html")
+
+
+
+# @login_required
+def home(request):
+	business_owner = BusinessOwner.objects.get(username=request.user.username)
+
+	# log out
+	if request.method == "POST":
+		if "logout-btn" in request.POST:
+			logout(request)
+			return redirect('/')
+			
+	return render(request, "home.html", 
+		{"username": business_owner.username, "business_name": business_owner.business_name})
+
+
+
+
