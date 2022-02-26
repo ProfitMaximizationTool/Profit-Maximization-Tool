9e2b00fdab4a81123ae8c77931f1485b67c0df11
Alyssa Beatrice Diokno <dioknobea@gmail.com>
2022-02-26 15:09:26 +0800

    Added script.js

---
+++ pages/js/script.js
@@ -0,0 +1,8 @@
+function opensignin() {
+    document.getElementById("signinoverlay").style.display = "block";
+}
+
+function closesignin() {
+    document.getElementById("signinoverlay").style.display = "none";
+}
+
