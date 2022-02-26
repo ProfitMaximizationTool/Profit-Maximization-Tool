af4561475b8398fa5db1c51f9c85230d1cc657ea
Alyssa Beatrice Diokno <dioknobea@gmail.com>
2022-02-26 16:55:45 +0800

    Functions for sign up and sign in buttons

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,3 +1,11 @@
+function opensignup() {
+    document.getElementById("signupoverlay").style.display = "block";
+}
+
+function closesignup() {
+    document.getElementById("signupoverlay").style.display = "none";
+}
+
 function opensignin() {
     document.getElementById("signinoverlay").style.display = "block";
 }
