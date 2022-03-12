50f5d57d528a454bd3fa542626e84812610e61e6
Hans De Castro <hhdecastro@up.edu.ph>
2022-03-12 21:18:04 +0800

    Fixed Product.ingredients default

--- ProfitMaximizationTool/profitmaximizer/models.py
+++ ProfitMaximizationTool/profitmaximizer/models.py
@@ -20,6 +20,6 @@ class IngredientRecord(models.Model):
 class ProductRecord(models.Model):
 	owner = models.ForeignKey(BusinessOwner,on_delete=models.CASCADE)
 	productName = models.CharField(max_length=255)
-	ingredients = models.JSONField(default=dict())
+	ingredients = models.JSONField(default=dict)
 	cost = models.DecimalField(max_digits=10,decimal_places=2)
 	price = models.DecimalField(max_digits=10,decimal_places=2)
